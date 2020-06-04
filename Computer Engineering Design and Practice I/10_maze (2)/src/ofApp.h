4b666e41139da3fabc80b72cbd17702268e476a0
vetan2 <vetan2@naver.com>
2020-06-04 20:43:07 +0900

    Add maze (2) assignment
    Add a new lecture.
    Add Maze (2) assignment for the 10th week.

---
+++ Computer Engineering Design and Practice I/10_maze (2)/src/ofApp.h
@@ -0,0 +1,86 @@
+/*
+
+	ofxWinMenu basic example - ofApp.h
+
+	Copyright (C) 2016-2017 Lynn Jarvis.
+
+	http://www.spout.zeal.co
+
+	=========================================================================
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	=========================================================================
+*/
+
+	#pragma once
+
+#include "ofMain.h"
+#include "ofxWinMenu.h" // Menu addon
+
+class ofApp : public ofBaseApp {
+
+	public:
+
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key); // Traps escape key if exit disabled
+		void keyReleased(int key);
+		void mouseMoved(int x, int y);
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		bool readFile();
+		void freeMemory();
+		bool DFS();
+		void dfsdraw();
+		int HEIGHT;//�̷��� ����
+		int WIDTH;//�̷��� �ʺ�
+		char** input;//�ؽ�Ʈ ������ ��� ������ ��� ������ �迭�̴�.
+		int** visited;//�湮���θ� ������ ����
+		int maze_col;//�̷�ĭ�� ���� �ε����� ����Ų��.
+		int maze_row;//�̷�ĭ�� ���� �ε����� ����Ų��.
+		int k;
+		int isOpen; //������ ���ȴ����� �Ǵ��ϴ� ����. 0�̸� �ȿ��Ȱ� 1�̸� ���ȴ�.
+		int isDFS;//DFS�Լ��� ������״��� �Ǵ��ϴ� ����. 0�̸� ������߰� 1�̸� �����ߴ�.
+		int isBFS;//BFS�Լ��� ������״��� �Ǵ��ϴ� ����. 0�̸� ������߰� 1�̸� �����ߴ�.
+		// Menu
+		ofxWinMenu * menu; // Menu object
+		void appMenuFunction(string title, bool bChecked); // Menu return function
+
+		// Used by example app
+		ofTrueTypeFont myFont;
+        ofImage myImage;
+		float windowWidth, windowHeight;
+		HWND hWnd; // Application window
+		HWND hWndForeground; // current foreground window
+
+		// Example menu variables
+		bool bShowInfo;
+		bool bFullscreen;
+		bool bTopmost;
+		bool isdfs;
+		// Example functions
+ 		void doFullScreen(bool bFull);
+		void doTopmost(bool bTop);
+
+		int N, M;				// 미로의 가로와 세로
+		int** maze = NULL;		// 미로 배열
+
+		int gridLen;			// 격자의 길이
+		bool drag_flag_maze = FALSE;
+};
