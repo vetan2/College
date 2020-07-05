660391b346f02c07439c0bfebc2eb600d0f2eb85
vetan2 <vetan2@naver.com>
2020-07-05 15:20:38 +0900

    Erase wrong comments

--- Computer Engineering Design and Practice I/term-end project/src/waterObj.cpp
+++ Computer Engineering Design and Practice I/term-end project/src/waterObj.cpp
@@ -48,8 +48,6 @@ state waterObj::getState(ofVec2f pos) {
 
 	tempPos = destPos = pos;
 	for (int lineIndex = 0; lineIndex < lineCnt; lineIndex++) {
-		// By comparing the y-coordinates of two dots forming a line,
-		//	the determine linePos_upper and linePos_lower
 		if (modifiedLine[lineIndex].pos1.y < modifiedLine[lineIndex].pos2.y) {
 			linePos_upper = modifiedLine[lineIndex].pos1;
 			linePos_lower = modifiedLine[lineIndex].pos2;
@@ -60,8 +58,6 @@ state waterObj::getState(ofVec2f pos) {
 			linePos_lower = modifiedLine[lineIndex].pos1;
 		}
 
-		// If the y-coordinates of the two dots are the same,
-		//	then determine linePos_upper and linePos_lower randomly
 		else {
 			random_device rd;
 			mt19937 gen(rd());
@@ -78,56 +74,42 @@ state waterObj::getState(ofVec2f pos) {
 			}
 		}
 
-		// If linePos_lower is above pos, then compare pos with the next line
 		if (pos.y - linePos_lower.y >= upsilon)
 			continue;
 
-		// 선분 위에 x좌표가 수도꼭지의 x좌표인 점이 없으면 continue
 		if (linePos_upper.x > linePos_lower.x) {
 			if (linePos_lower.x - pos.x >= upsilon || pos.x - linePos_upper.x >= upsilon)
 				continue;
 		}
-
 		else
 			if (linePos_upper.x - pos.x >= upsilon || pos.x - linePos_lower.x >= upsilon)
 				continue;
 
-		// 선분 위에서 x좌표가 수도꼭지의 x좌표인 점을 찾음
 		ofVec2f u(linePos_lower.x - linePos_upper.x, linePos_lower.y - linePos_upper.y);
 		float t = (pos.x - linePos_upper.x) / u.x;
 		tempPos = ofVec2f(linePos_upper.x + u.x * t, linePos_upper.y + u.y * t);
 
-		// 찾은 점이 curCor과 일치하는가? (upsilon과의 비교)
+		
 		if (tempPos.distance(pos) < upsilon)
-
-			// 일치하는 경우(upsilon과의 비교),
 			if (tempPos.distance(linePos_lower) >= upsilon) {
-
-				// 그 점이 아랫점이 아니면, temp에 아랫점을 저장, return oblique
 				destPos = linePos_lower;
 				return diagnal;
 			}
 
-		// 일치하지 않는 경우
-		// 찾은 점이 현재 위치보다 아래에 있을 때
-		if (tempPos.y > pos.y)
-
-			// tempCor이 초기화상태 그대로라면, tempCor에 찾은 점을 저장
+		if (tempPos.y > pos.y) {
 			if (destPos == pos)
 				destPos = tempPos;
-
-		// tempCor이 초기화상태가 아니라면, 찾은 점과 tempCor 중 더 위에 있는 점을 tempCor에 저장
 			else
 				if (destPos.y > tempPos.y)
 					destPos = tempPos;
+		}
 	}
+	// If passed loop above, pos is not on any line
 
-	// 위의 for문을 통과했으면 현재 위치는 선분 위에 있는 것이 아님
-	// tempCor이 초기화상태 그대로가 아니라면
 	if (destPos != pos)
 		return vertical;
 
-	// tempCor이 초기화상태 그대로라면 현재 물 위치 아래에 아무것도 없는 것
+	// If destPos is in the initial state, there is nothing under pos
 	return verticalTillEnd;
 }
 
@@ -139,30 +121,18 @@ void waterObj::moveVertically() {
 	while (1) {
 		velocity.y += g;
 		nextPos.y += velocity.y;
-		// y와 tempCor[1]이 일치하지 않으면 (upsilon 비교)
 		if (nextPos.distance(destPos) > upsilon) {
-
-			// y가 tempCor[1]보다 아래에 있으면
 			if (nextPos.y > destPos.y) {
-
-				// pathCor과 curCor에 tempCor을 저장 후 return
 				path[pathCnt++] = destPos;
 				return;
 			}
-
-			// y가 tempCor[1]보다 위에 있으면
 			else {
-
-				// pathCor에 x = curCor[0], y = y인 점을 저장
 				path[pathCnt] = path[pathCnt - 1];
 				path[pathCnt++].y = nextPos.y;
 			}
 		}
 
-		// y와 tempCor[1]이 일치하면
 		else {
-
-			// pathCor에 tempCor을 저장 후 return
 			path[pathCnt++] = destPos;
 			return;
 		}
@@ -201,29 +171,17 @@ void waterObj::moveDiagnally() {
 		velocity *= ((velocity.length() + g * sin(ofVec2f(1, 0).angleRad(velocity))) / velocity.length());
 		nextPos += velocity;
 
-		// y와 tempCor[1]이 일치하지 않으면 (upsilon 비교)
 		if (nextPos.distance(destPos) >= upsilon) {
-
-			// y가 tempCor[1]보다 아래에 있으면
 			if (nextPos.y > destPos.y) {
-
-				// pathCor과 curCor에 tempCor을 저장 후 return
 				path[pathCnt++] = destPos;
 				return;
 			}
 
-			// y가 tempCor[1]보다 위에 있으면
-			else {
-
-				// pathCor에 x = x, y = y인 점을 저장
+			else
 				path[pathCnt++] = nextPos;
-			}
 		}
 
-		// y와 tempCor[1]이 일치하면
 		else {
-
-			// pathCor과 curCor에 tempCor을 저장 후 return
 			path[pathCnt++] = destPos;
 			return;
 		}
